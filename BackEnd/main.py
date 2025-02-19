3073e037c9e2bc57fece19cf0c6b2ebac16275eb
Igor Rahzel <igorrahzel@gmail.com>
2025-02-19 11:30:22 -0300

    updated readme

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,68 +1,83 @@
-import cv2
 from ultralytics import YOLO
+import cv2
 import numpy as np
 import torch
 
 # Paths
 model_path = 'BackEnd/model/best_segment.pt'
 video_path = 'BackEnd/IMG_8443.MOV'
-output_video_path = 'output_video_with_mask.mp4'
 
-# Load YOLO model
-model = YOLO(model_path)
+# Initialize YOLO model
+model = YOLO(model_path,task='segment')
 print(model.model.task)
 
-# Open video file
-cap = cv2.VideoCapture(video_path)
-fps = cap.get(cv2.CAP_PROP_FPS)  # Obtém a taxa de quadros por segundo do vídeo
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # Número total de frames no vídeo
-duration_seconds = 15  # Duração do vídeo de saída em segundos
-frames_to_capture = int(fps * duration_seconds)  # Número de frames que correspondem a 15 segundos
+def get_mask(frame,model):
+    #frame = cv2.resize(frame,(640,480))
+    results = model(frame)  # No resizing
 
-# Verifique se o vídeo é grande o suficiente para gerar 15 segundos de vídeo
-if frame_count < frames_to_capture:
-    print("O vídeo não tem frames suficientes para gerar 15 segundos de vídeo.")
-    exit()
+    if results[0].masks is None:
+        print("No masks detected (results[0].masks is None)")
+        return np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
 
-# Cria um vídeo de saída
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec de vídeo
-out = cv2.VideoWriter(output_video_path, fourcc, fps, (640, 480))  # Define as configurações do vídeo de saída
+    masks = results[0].masks.data if results[0].masks is not None else None
 
-count = 0
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break  # Stop if no frames left
+    if masks is None or masks.shape[0] == 0:
+        print("No masks found in results (masks.shape[0] == 0)")
+        return np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
 
-    print(f'Frame Shape: {frame.shape}')
+    print(f"Detected {masks.shape[0]} masks with shape {masks.shape}")
 
-    # Run segmentation
-    results = model(frame)
+    # Combine masks into a single binary image
+    combined_mask = torch.any(masks, dim=0).int().cpu().numpy()
+    
+    return combined_mask.astype(np.uint8)
 
-    # Extract masks
-    masks = results[0].masks.data  # Shape: (N, H, W)
 
-    if masks.shape[0] > 0:  # Ensure at least one detection exists
-        # Combinar todas as máscaras em uma única binária
-        combined_mask = torch.any(masks, dim=0).int().cpu().numpy()  # Convert para NumPy
-        print(f'Combined Mask Shape: {combined_mask.shape}')
+def segmented_frames(frame, model):
+    
+    bin_mask = get_mask(frame,model)
+    print(f'bin_mask.shape: {bin_mask.shape}')
+    bin_mask = cv2.resize(bin_mask,(640,480))
+    frame = cv2.resize(frame, (640, 480))
+    
+    #colored frame
+    colored_frame = np.zeros_like(frame)
+    colored_frame[:,:] = (255,0,255)
+    alpha = 0.5
+    bin_mask = bin_mask.astype(bool)
+    frame[bin_mask] = cv2.addWeighted(frame,alpha,colored_frame,1-alpha,0.0)[bin_mask]
 
-        # Converter frame para GrayScale
-        frame_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.resize(frame_gray,(640,384))
-        print(f'Frame Gray Shape: {frame_gray.shape}')
-        print(np.unique(combined_mask))
-        combined_mask = combined_mask.astype(np.uint8)
-        imageInMask = combined_mask * frame_gray
-        cv2.imwrite(f'output_image_in_mask_{count}.png',imageInMask)
-        # Adiciona o frame processado ao vídeo de saída
-        #out.write(frame)
+    return frame
 
-        #print(f'Processed Frame {count}')
+def segmented_mask(frame, model):
+    
+    combined_mask = get_mask(frame,model)
+    combined_mask *= 255
+    combined_mask = cv2.resize(combined_mask,(480,320))
+    #Area
+    area = np.sum(combined_mask > 0)
 
-        break  # Stop after capturing 15 seconds of frames
 
-# Cleanup
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    return combined_mask.astype(np.uint8),area  # Ensure uint8 format
+
+def read_video(video_path, model):
+    cap = cv2.VideoCapture(video_path)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        segmented_frame = segmented_frames(frame, model)
+        segmented_mask_frame,area = segmented_mask(frame, model)
+
+        cv2.imwrite('segmented_frame.jpg', segmented_frame)
+        cv2.imwrite('segmented_mask_frame.jpg', segmented_mask_frame)
+        cv2.imwrite('frame.jpg', frame)
+
+        break
+        
+    cap.release()
+
+
+read_video(video_path, model)
